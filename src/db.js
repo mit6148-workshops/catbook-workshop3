5846e9e60e2b025150e876e5ed76b2f6b767d72f
Robert Vunabandi <rvunabandi@gmail.com>
2018-01-10 16:32:27 -0500

    made changes to avoid crash

--- src/db.js
+++ src/db.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 // setup mongoDB connection
-const mongoURL = "mongodb://robert:qwerty123@dsXXXXXX.mlab.com:XXXXX/catbookdb";
+const mongoURL = "mongodb://robertv:qwerty123@ds251287.mlab.com:51287/catbookdb";
 const options = {
     useMongoClient: true
 };
-mongoose.connet(mongoURL, options);
+mongoose.connect(mongoURL, options);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 // db error handling
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// optional connection handling: feedback for us to know that we're connected
-db.on('connected', console.log('database connected!'));
+db.on('connected', function() { console.log('database connected'); });
 module.exports = db;

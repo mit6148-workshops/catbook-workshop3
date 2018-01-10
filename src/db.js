5455a4dd49c722e1bc56a9978c94ffcdc83df294
Robert Vunabandi <rvunabandi@gmail.com>
2018-01-10 16:36:13 -0500

    made fix on db.js

--- src/db.js
+++ src/db.js
@@ -5,12 +5,12 @@ const mongoURL = "mongodb://robert:qwerty123@dsXXXXXX.mlab.com:XXXXX/catbookdb";
 const options = {
     useMongoClient: true
 };
-mongoose.connet(mongoURL, options);
+mongoose.connect(mongoURL, options);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 // db error handling
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // optional connection handling: feedback for us to know that we're connected
-db.on('connected', console.log('database connected!'));
+db.on('connected', function() { console.log('database connected!'); });
 module.exports = db;

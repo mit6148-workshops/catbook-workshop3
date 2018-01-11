7cb512525ed0cd975e08f1ef9220f94544e419a6
Robert Vunabandi <rvunabandi@gmail.com>
2018-01-10 19:40:24 -0500

    made it so that people can post comments

--- public/js/feed.js
+++ public/js/feed.js
@@ -27,6 +27,9 @@ function storyDOMObject(storyJSON, user) {
   commentsDiv.className = 'story-comments';
   cardFooter.appendChild(commentsDiv);
 
+  // added this line to allow adding new comments to a story
+  cardFooter.appendChild(newCommentDOMObject(storyJSON._id));
+
   return card;
 }
 
